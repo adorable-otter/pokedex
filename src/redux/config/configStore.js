770c58a7a30367c1193824d5b5b956fcec3cefe0
adorable-otter <plan.stack@gmail.com>
2024-11-10 22:18:19 +0900

    sonner를 통한 에러 메시지 출력 적용

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedPokemonSlice from '../slices/selectedPokemonSlice';
+import { toast } from 'sonner';
+
+const errorMiddleware = () => (next) => (action) => {
+  try {
+    return next(action);
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
 const store = configureStore({
   reducer: {
     selectedPokemonList: selectedPokemonSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(errorMiddleware),
 });
 
 export default store;
